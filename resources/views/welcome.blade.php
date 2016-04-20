49e815617cae87bea1883fd93e6f18fe8b7bf843
puccino <ricohombre88@gmail.com>
2016-04-20 20:16:21 +0300

    April 20th 2016

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,23 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                 <font color="green">
+                 <center>Tanzania Social Action Fund - PSSN Operations Monitoring System</font></center>
+                 </font>
+                 </div>
+
+                <div class="panel-body">
+                <img src="{{ 'assets/images/logo.jpg' }}">
+                <link rel='shortcut icon' type='image/x-icon' href='logo.ico' />
+                    Your Application's Landing Page.
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
